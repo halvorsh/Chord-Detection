d914827c76dc4f497ada0269c98dffc3a9bb53c1
Halvor Sogn Haug <halvorsh@gmail.com>
2021-07-02 11:33:29 +0200

    Will voice leading work?

--- voice_leading.py
+++ voice_leading.py
@@ -12,13 +12,14 @@ ALL_NOTES_OFF = [0, 47, 97, 108, 108, 95, 110, 111, 116, 101, 115, 95, 111, 102,
 BLINK = [0, 47, 98, 108, 105, 110, 107]
 PORT = mido.open_output('Dr Squiggles:Dr Squiggles MIDI 1 20:0')
 
-"""tree = ET.parse('../.squiggles_notes/squiggles_notes.xml')
+tree = ET.parse('../.squiggles_notes/squiggles_notes.xml')
 root = tree.getroot()
-NOTES = [int(root[i].text.strip()) for i in range(1,9)]"""
-NOTES = [73, 66, 58, 54, 53, 61, 65, 72]
+NOTES = [int(root[i].text.strip()) for i in range(1,9)]
+PREVIOUS_NOTE = None
 print(NOTES)
 
 def convert_note_to_solinoid(note):
+    global PREVIOUS_NOTE
     midi_note = (note-4)%12+52
     note_to_play = None
     octaves = [0,1]
@@ -26,6 +27,7 @@ def convert_note_to_solinoid(note):
     for i in octaves:
         tested_note = midi_note+12*i
         if tested_note in NOTES:
+            PREVIOUS_NOTE = tested_note
             note_to_play = NOTES.index(tested_note)
             break
     if note_to_play != None:
@@ -48,11 +50,45 @@ def stop_all():
     PORT.send(msg)
 
 def play_chord(chord):
+    global PREVIOUS_NOTE
     notes_in_chord = [i for i, x in enumerate(chord) if x == 1]
     np.random.shuffle(notes_in_chord)
     for note in notes_in_chord:
         if convert_note_to_solinoid(note):
             break
+
+def voice_leading(chord):
+    global PREVIOUS_NOTE
+
+    if PREVIOUS_NOTE == None:
+        play_chord(chord)
+
+    notes_in_chord = [i for i, x in enumerate(chord) if x == 1]
+    possible_notes = []
+    for note in notes_in_chord:
+        midi_note = (note-4)%12+52
+        for i in range(2):
+            tested_note = midi_note+12*i
+            if tested_note in NOTES:
+                possible_notes.append(tested_note)
+
+    if len(possible_notes) == 0:
+        PREVIOUS_NOTE = None
+        return
+
+    min_distance = 24
+    best_note = None
+    for note in possible_notes:
+        dist = np.abs(PREVIOUS_NOTE - note)
+        if dist < min_distance:
+            best_note = note
+
+    print(PREVIOUS_NOTE, best_note, NOTES)
+    PREVIOUS_NOTE = best_note
+    note_to_play = NOTES.index(best_note)
+    play_solinoid(note_to_play)
+
+
 
 CHUNK = 2**15
 RATE = 44100
@@ -81,9 +117,7 @@ while True:
             root = index_to_note[pred%12]
             type = type_of_chord[int(pred//12)]
             print(root, type)
-            #convert_note_to_solinoid(pred%12)
-            play_chord(chord.chord_profiles[pred])
-
+            voice_leading(chord.chord_profiles[pred])
 stream.stop_stream()
 stream.close()
 p.terminate()
